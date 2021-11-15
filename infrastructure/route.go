b24094d93d9f67bf0aaf064ded12ce663d3a74fd
miguel soto <miguelsr.1084@gmail.com>
2021-11-15 14:30:04 -0500

    integracion de hablder con la ruta terminada

--- infrastructure/route.go
+++ infrastructure/route.go
@@ -1,1 +1,28 @@
 package infrastructure
+
+import (
+	"github.com/MikelSot/Ed-api/interfaces"
+	"github.com/MikelSot/Ed-api/middleware"
+	"github.com/labstack/echo"
+)
+
+type route struct {
+	echo *echo.Echo
+	data interfaces.Data
+}
+
+func NewRoute(e *echo.Echo, d interfaces.Data) *route {
+	return &route{e, d}
+}
+
+func (r route) Routes()  {
+	handler := newCourse(r.data)
+	course := r.echo.Group("/v1/course")
+	course.Use(middleware.Log)
+
+	course.POST("/create", handler.create)
+	course.PUT("/create", handler.update)
+	course.GET("/get-by-id/:id", handler.getById)
+	course.GET("/get-all", handler.getAll)
+}
+
